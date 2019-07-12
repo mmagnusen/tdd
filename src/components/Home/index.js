c44d19f70f896f425631912dd068736ee9e36fdb
Marilyn Magnusen <m-mags@hotmail.co.uk>
2019-07-12 17:11:36 +0100

    styles and jest content

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -8,6 +8,7 @@ class Home extends Component {
         return (
             <div className='Home'>
                 <section className='container'>
+                <h1>Home</h1>
                     <section className='white-content'>
                         <h1>What is testing?</h1>
                         <p>The aim of testing is to minimise bugs in production and to make sure your application behaves as you expect it to.
@@ -15,17 +16,17 @@ class Home extends Component {
                     </section>
                     <section className='white-content'>
                     <h1>What is Test Driven Development?</h1>
-                    <p>Test Driven Development (TDD) treats testing as part of the development process, rather than an after-thought</p>
+                    <p>Test Driven Development (TDD) treats testing as part of the development process, rather than an after-thought. You write the test first, and then you write code to make that test pass.</p>
                     </section>
                     <section className='white-content'>
                         <h1>What should we test?</h1>
                         <p>
-                            We do not need to test functions created by third party libraries, these have already been tested in those libraries.
-                            We should only test code we write ourselves
+                            We do not need to test code written by third party libraries, these have already been tested in those libraries.
+                            We should only test code we write ourselves.
                         </p>
                     </section>
                     <section className='white-content'>
-                    <h1>What can we implement tests?</h1>
+                    <h1>What can we use to implement tests?</h1>
                     <p>
                         There is a wide variety of front-end frameworks for testing. One of the most popular is <Link type={LINK_TYPE.EXTERNAL} to='https://jestjs.io/'>Jest</Link>.
                     </p>
--- src/components/Jest/index.js
+++ src/components/Jest/index.js
@@ -12,24 +12,25 @@ class Jest extends Component {
                     <h1>Jest</h1>
                     <section className='white-content'>
                         <h1>What is Jest?</h1>
-                        <p>Jest is a test runner. This means that it runs the tests you have written and tells you if the code passes or fails those tests.</p>
-                        <p>It's created by the team at Facebook</p>
-                        <p>Comes pre-installed in every create-react-app application</p>
-                        <p>Works with many front-end frameworks including React, Vue and many more</p>
+                        <p><Link to='https://jestjs.io/' type={LINK_TYPE.EXTERNAL}>Jest</Link> is a test runner. This means that it runs the tests you have written and tells you if the code passes or fails those tests.</p>
+                        <p>It's created by the team at <Link to='https://www.facebook.com/' type={LINK_TYPE.EXTERNAL}>Facebook</Link> </p>
+                        <p>It comes pre-installed in every <Link to='https://www.npmjs.com/package/create-react-app' type={LINK_TYPE.EXTERNAL}>create-react-app</Link> application</p>
+                        <p>Works with many front-end frameworks including <Link to='https://reactjs.org/' type={LINK_TYPE.EXTERNAL}>React</Link>, <Link to='https://vuejs.org/' type={LINK_TYPE.EXTERNAL}>Vue</Link> and many more</p>
                     </section>
 
                     <section className='white-content'>
                         <h1>Why is it useful?</h1>
-                        <p>Tells you code coverage - this means you know how much of your code is covered by tests. The higher this number, the better.</p>
+                        <p>Provides an overview of your code coverage - this means you know how much of your code is covered by tests. Generally, the higher this number, the better.</p>
                     </section>
 
                     <section className='white-content'>
                         <h1>Starting with Jest</h1>
 
-                        <p>Writing our first test: <Link to='https://github.com/mmagnusen/calculator' type={LINK_TYPE.EXTERNAL}>https://github.com/mmagnusen/calculator</Link></p>
+                        <p>Writing our first test - you can see the full code here: <Link to='https://github.com/mmagnusen/calculator/blob/master/src/calculator.test.js' type={LINK_TYPE.EXTERNAL}>https://github.com/mmagnusen/calculator</Link></p>
+                        <p>This is an example of a test:</p>
                         <CodeBlock>
 {`
-    describe('calculateArea', () => {
+    describe('calculate the area', () => {
     
         const calculateArea = (length, width) => {
             return length * width;
@@ -46,12 +47,12 @@ class Jest extends Component {
                         </CodeBlock>
 
                         <h1>What is happening here?</h1>
-                        <p>'describe' is a method. It accepts two parameters. The first parameter is a short description of the test</p>
+                        <p><span className='variable'>describe()</span> is a method. It accepts two parameters. The first parameter is a short description of the test. We have written our description as <span className='variable'>calculate the area</span></p>
                         <p>The second parameter is a function, which will be called by Jest. Inside this function, we can outline what we want to test to do and say what we expect the result to be.</p>
-                        <p>First we created a function, called calculateArea. It will be passed a length and a width and should return the length * width to calculate the total area.</p>
-                        <p>We now call that function, passing it a length of 2 and a width of 2. We assign the result of this function call to a constant named area.</p>
-                        <p>`Expect` is a function, which should be passed a value. This can be any type of value, in our case it is a number.</p>
-                        <p>`Expect` will always be used with a matcher, which lets you validate what the value should be. We are saying we expect the value (area) to be 4.</p>
+                        <p>First we created a function, called <span className='variable'>calculateArea</span>. It will be passed a length and a width and should return the length * width to calculate the total area.</p>
+                        <p>We now call that function, passing it a length of 2 and a width of 2. We assign the result of this function call to a constant named <span className='variable'>area</span>.</p>
+                        <p><span className='variable'>expect()</span> is a function, which should be passed a value. This can be any type of value, in our case it is a number.</p>
+                        <p><span className='variable'>expect()</span> will always be used with a matcher, which lets you validate what the value should be. We are saying we expect the value <span className='variable'>area</span> to be 4.</p>
                     </section>
 
                     <section className='white-content'>
